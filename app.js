7ff6690b852e5500aeb27b970952ed401ba6162e
iqballbaihaqi <baihaqiiqbal323@gmail.com>
2021-01-29 05:45:38 +0700

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = procces.env.PORT || 3000;
 const route = require('./routes');
 const session = require('express-session')
 const rupiah = require('./helpers/rupiah')
